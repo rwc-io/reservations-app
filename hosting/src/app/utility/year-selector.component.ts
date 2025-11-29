0186eabe41e8ea1d671658a0b253583d4dc23ae7
David Haley <dchaley@gmail.com>
2025-11-28 21:16:04 -0800

    fixup to extract

---
+++ hosting/src/app/utility/year-selector.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, model, Signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {MatFormField} from '@angular/material/form-field';
+import {MatSelect, MatOption} from '@angular/material/select';
+import {MatLabel} from '@angular/material/form-field';
+
+@Component({
+  selector: 'year-selector',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgForOf,
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    MatOption,
+  ],
+  template: `
+    <mat-form-field>
+      <mat-label>{{ label }}</mat-label>
+      <mat-select [(ngModel)]="selectedYear">
+        <mat-option *ngFor="let year of yearsAsArray()" [value]="year">
+          {{ year }}
+        </mat-option>
+      </mat-select>
+    </mat-form-field>
+  `,
+})
+export class YearSelectorComponent {
+  @Input() label = 'Viewing year';
+  @Input() years: number[] | Signal<number[]> = [];
+
+  // Two-way bound from parent
+  selectedYear = model<number>(new Date().getFullYear());
+
+  yearsAsArray(): number[] {
+    return Array.isArray(this.years) ? this.years : (this.years as Signal<number[]>)();
+  }
+}
