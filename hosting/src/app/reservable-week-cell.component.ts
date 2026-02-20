c0961144c47abaeadefbe4f3261830bc6a612dc1
David Haley <dchaley@gmail.com>
2026-02-19 16:40:54 -0800

    Move guest/group grid info into chips

--- hosting/src/app/reservable-week-cell.component.ts
+++ hosting/src/app/reservable-week-cell.component.ts
@@ -1,12 +1,13 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
 import {MatAccordion, MatExpansionPanel, MatExpansionPanelHeader} from '@angular/material/expansion';
 import {MatList, MatListItem, MatListItemLine, MatListItemTitle} from '@angular/material/list';
+import {MatChip, MatChipSet, MatChipTrailingIcon} from '@angular/material/chips';
 import {DateTime} from 'luxon';
 import {BookableUnit, Booker} from './types';
-import {WeekRow, WeekReservation} from './week-table.component';
+import {WeekReservation, WeekRow} from './week-table.component';
 import {ShortDate} from './utility/short-date.pipe';
 
 @Component({
@@ -23,7 +24,10 @@ import {ShortDate} from './utility/short-date.pipe';
     MatListItem,
     MatListItemLine,
     MatListItemTitle,
+    MatChip,
     ShortDate,
+    MatChipSet,
+    MatChipTrailingIcon,
   ],
   templateUrl: './reservable-week-cell.component.html',
   styleUrl: './reservable-week-cell.component.scss'
@@ -37,7 +41,7 @@ export class ReservableWeekCellComponent {
   @Input({required: true}) canAddDailyReservation!: boolean;
   @Input({required: true}) canEditReservationFn!: (reservation: WeekReservation) => boolean;
 
-  @Output() addReservation = new EventEmitter<{startDate: DateTime; endDate: DateTime}>();
+  @Output() addReservation = new EventEmitter<{ startDate: DateTime; endDate: DateTime }>();
   @Output() editReservation = new EventEmitter<WeekReservation>();
 
   isReservedByDay(reservations: WeekReservation[]): boolean {
