6c665b79e779c6318f1d1d5a267d86aea92fecbb
David Haley <dchaley@gmail.com>
2026-02-25 17:53:10 -0800

    Fix test & run tests during actions

--- hosting/src/app/app.component.spec.ts
+++ hosting/src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {Title} from '@angular/platform-browser';
+import reservationsAppConfig from './reservations-app.config.json';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -14,16 +16,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'reservations-app' title`, () => {
+  it(`should have the title from config`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('reservations-app');
+    fixture.detectChanges();
+    const titleService = TestBed.inject(Title);
+    expect(titleService.getTitle()).toEqual(reservationsAppConfig.applicationTitle);
   });
 
-  it('should render title', () => {
+  it('should render nothing in the template (only router-outlet)', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, reservations-app');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
